7928ce4beb5a2552e60e76009374bcab6e587ffb
Andreyy96 <taurussilver777@gmail.com>
2025-03-20 20:37:24 +0200

    frontend: the ability to go to the orders page if you have an access token

---
+++ frontend/src/hoc/CurrentUserLogined.tsx
@@ -0,0 +1,25 @@
+import {FC, PropsWithChildren, useEffect} from 'react';
+
+import {Navigate} from "react-router-dom";
+import {authService} from "../services/authService.ts";
+
+
+interface IProps extends PropsWithChildren {}
+
+const CurrentUserLogined:FC<IProps> = ({children}) => {
+
+    const accessToken = authService.getAccessToken()
+    ;
+
+    if (accessToken) {
+        return <Navigate to={"/orders"}/>
+    }
+
+    return (
+        <>
+            {children}
+        </>
+    );
+};
+
+export {CurrentUserLogined};
